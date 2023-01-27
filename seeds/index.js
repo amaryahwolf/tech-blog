b1f74ab769f646562b925b60ce2f1e7196b0b59e
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-01-27 08:10:01 -0800

    add home routes

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const sequelize = require('../config/connection');
+
 const seedUsers = require('./userData');
 const seedPosts = require('./postData');
 const seedComments = require('./commentData');
